75274bcf9684a5b66258dc9cd21023a2842b4244
leidanium <alexsam.quentin@gmail.com>
2021-12-10 15:46:08 +0100

    fix error

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,13 @@
 class Article < ApplicationRecord
-    has_and_belongs_to_many :categories, dependant: :delete_all
-    has_many :article_characteristics, dependent: :delete_all
-    # has_many :characteristics, through: :article_characteristics
+
+    has_many :article_categories
+    has_many :categories, dependant: :delete_all
+
+    has_many :article_characteristics, dependent: :destroy
+    has_many :characteristics, through: :article_characteristics
+
     has_one :address
-    belongs_to :article
+    belongs_to :user
 
     enum condition: ["neuf", "bon etat", "pour piece"]
 end
