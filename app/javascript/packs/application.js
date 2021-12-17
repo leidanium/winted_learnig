ea78a585718305910ca19e5e4dde33f09f49c124
leidanium <alexsam.quentin@gmail.com>
2021-12-17 15:53:22 +0100

    Rails app:update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+
+import 'bootstrap'
+import '../stylesheets/application'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,6 +23,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import 'bootstrap'
-import '../stylesheets/application'
