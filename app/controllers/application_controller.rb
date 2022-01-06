2b7fac75c980f1df30d2b7126a18087300486a46
leidanium <alexsam.quentin@gmail.com>
2022-01-06 14:26:15 +0100

    Profile User Img

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
     protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:pseudo, :tel])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:pseudo, :tel, :avatar])
   end
 end
