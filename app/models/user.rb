71a556170c4745337fb02435d146d9d20a871f13
leidanium <alexsam.quentin@gmail.com>
2022-01-06 14:25:53 +0100

    Add profile ing to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  
+
   has_many :articles
+
+  has_one_attached :avatar
 end
